9f807c873806c052289827a9a01699b70f676a64
Joel Garcia <garciajunior796@gmail.com>
2024-07-20 01:13:33 -0500

    Adding compiler

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "VERIFYING THE FOLDER 'backups' EXISTS"
+
+if [ ! -d "backups" ]; then
+	echo "CREATING FOLDER 'backups'"
+	mkdir backups
+fi
+
+echo "VERIFYING THE FOLDER 'logs' EXISTS"
+
+if [ ! -d "logs" ]; then
+	echo "CREATING FOLDER 'logs'"
+	mkdir logs
+fi
+
+echo "COMPILING PROFUGO"
+cargo build
+
+echo "DONE"
+
